c92da929f00d69555235b8a44e9f6a06ba72d771
andersonmsilva01 <andersommsilva55@gmail.com>
2026-03-03 21:34:02 -0300

    update

--- projeto_django/escola/alunos/tests.py
+++ projeto_django/escola/alunos/tests.py
@@ -12,20 +12,20 @@ class AlunoModelTest(TestCase):
         self.assertEqual(aluno.nome, "Teste")
         self.assertTrue(aluno.matriculado)
 
+    def test_idade_aluno(self):
+        aluno = Aluno.objects.create(
+            nome="Carlos",
+            idade=25,
+            email="carlos@email.com"
+        )
+        self.assertEqual(aluno.idade, 25)
+
 class AlunoViewTest(TestCase):
 
     def test_lista_alunos_status(self):
         response = self.client.get('/alunos/')
         self.assertEqual(response.status_code, 200)
 
-def test_idade_aluno(self):
-    aluno = Aluno.objects.create(
-        nome="Carlos",
-        idade=25,
-        email="carlos@email.com"
-    )
-    self.assertEqual(aluno.idade, 25)
-
-def test_rota_novo_aluno(self):
-    response = self.client.get('/alunos/novo/')
-    self.assertEqual(response.status_code, 302)
+    def test_rota_novo_aluno(self):
+        response = self.client.get('/alunos/novo/')
+        self.assertEqual(response.status_code, 302)
